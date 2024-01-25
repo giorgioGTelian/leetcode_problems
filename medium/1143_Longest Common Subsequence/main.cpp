27a90af14e13f8274d5ad4948dcb405395c34e76
giorgio <giorgio.programmer@gmail.com>
2024-01-25 13:35:27 +0100

    Create main.cpp

---
+++ medium/1143_Longest Common Subsequence/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isSubSequence(string text1, string text2, int m, int n) {
+        // Base Cases
+        if (m == 0) return true;
+        if (n == 0) return false;
+
+        // If last characters of two strings are matching
+        if (text1[m - 1] == text2[n - 1])
+            return isSubSequence(text1, text2, m - 1, n - 1);
+
+        // If last characters are not matching
+        return isSubSequence(text1, text2, m, n - 1);
+    }
+
+     int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
